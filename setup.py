a71b5f93ab61686a5b4233291eb6d6b613e3a915
Igor Cotruta <Hugoberry@users.noreply.github.com>
2025-02-26 15:44:29 +0000

    OpenMP on macOS absolute

--- setup.py
+++ setup.py
@@ -24,9 +24,12 @@ if not is_macos:
     extra_compile_args.append("-fopenmp")
     extra_link_args.append("-fopenmp")
 else:
-    # macOS: Use `libomp` (only if installed)
-    extra_compile_args.extend(["-Xpreprocessor", "-fopenmp"])
-    extra_link_args.extend(["-lomp"])
+    # macOS: Use Homebrew's libomp
+    omp_include_path = "/opt/homebrew/opt/libomp/include"
+    omp_lib_path = "/opt/homebrew/opt/libomp/lib"
+    
+    extra_compile_args.extend(["-Xpreprocessor", "-fopenmp", f"-I{omp_include_path}"])
+    extra_link_args.extend(["-lomp", f"-L{omp_lib_path}"])
 
 # Define the extension
 xpress9_module = Extension(
